3f39bc0d52dd46c288a2607b06319c01273e0c50
Ryan Bates <rbates214@gmail.com>
2018-06-01 17:20:21 -0500

    opens a whole new window

--- Assets/Scripts/RyanTools/NodeEditorWindow.cs
+++ Assets/Scripts/RyanTools/NodeEditorWindow.cs
@@ -1,18 +1,15 @@
 ﻿using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.Experimental.UIElements;
-using UnityEngine.XR.WSA.Input;
 
 namespace RyanTools
 {
     public class NodeEditorWindow : EditorWindow
     {
         private List<Node> _nodes;
-        
+
         /// <summary>
         ///     this creates the window out of the tab that was created from this.
-        ///
         ///     DONT MESS WITH THIS ANYMORE
         /// </summary>
         [MenuItem("Ryan's/Ryan's Window")]
@@ -40,13 +37,14 @@ namespace RyanTools
             _nodes.ForEach(n => n.PollEvents(Event.current));
             _nodes.ForEach(n => n.Draw());
 
+            if (GUILayout.Button(new GUIContent("Open New Window"), EditorStyles.toolbarButton, GUILayout.Width(1400)))
+                OpenNewWindow();
             if (GUILayout.Button(new GUIContent("Restart"), EditorStyles.toolbarButton, GUILayout.Width(1400)))
                 Restart();
         }
 
         /// <summary>
         ///     takes care of all the inputs or other events.
-        ///
         ///     DONT MESS WITH THIS ANYMORE
         /// </summary>
         /// <param name="e"></param>
@@ -64,7 +62,6 @@ namespace RyanTools
         /// <summary>
         ///     makes a context menu that gives
         ///     one option to Add Node.
-        ///
         ///     STILL NEED TO ADD HOW TO REMOVE A SINGLE NODE
         /// </summary>
         /// <param name="e"></param>
@@ -78,19 +75,16 @@ namespace RyanTools
             gm.AddItem(new GUIContent("Remove All Nodes"), false, RemoveAllNodes, e);
             gm.ShowAsContext();
 
-            for (int i = 0; i <= _nodes.Count; i++)
-            {
+            for (var i = 0; i <= _nodes.Count; i++)
                 if (_nodes[i].NodeRect.Contains(e.mousePosition))
                 {
                     gm.AddItem(new GUIContent("REMOVE NODE"), false, RemoveNode, e);
                     gm.ShowAsContext();
                 }
-            }
-    }
+        }
 
         /// <summary>
         ///     this is where you add the nodes to the screen from the mouse position.
-        ///
         ///     FINISHED
         /// </summary>
         /// <param name="e"></param>
@@ -103,7 +97,6 @@ namespace RyanTools
         /// <summary>
         ///     this allows you to Add the node to the screen by changing it from
         ///     a list item to an object.
-        ///
         ///     FINISHED
         /// </summary>
         /// <param name="obj"></param>
@@ -111,11 +104,10 @@ namespace RyanTools
         {
             AddNode(obj as Event);
         }
-        
+
         /// <summary>
         ///     allows you to remove all nodes from the list
-        ///
-        ///     FINISHED 
+        ///     FINISHED
         /// </summary>
         /// <param name="obj"></param>
         private void RemoveAllNodes(object obj)
@@ -125,19 +117,14 @@ namespace RyanTools
 
         /// <summary>
         ///     these allow you to remove one node at a time.
-        ///
         ///     THESE SHOULD BE DONE NEED TO TEST
         /// </summary>
         /// <param name="n"></param>
         private void RemoveNode(Event e)
         {
-            for (int i = 0; i <= _nodes.Count; i++)
-            {
+            for (var i = 0; i <= _nodes.Count; i++)
                 if (_nodes[i].NodeRect.Contains(e.mousePosition))
-                {
-                    _nodes.Remove(this._nodes[i]);
-                }
-            }
+                    _nodes.Remove(_nodes[i]);
         }
 
         private void RemoveNode(object obj)
@@ -145,9 +132,20 @@ namespace RyanTools
             RemoveNode(obj as Event);
         }
 
+        /// <summary>
+        ///     this allows you to reset your window to how it looked when you first opened it.
+        /// </summary>
         private void Restart()
         {
             OnEnable();
+        }
+
+        /// <summary>
+        ///     this allows you to open a whole new window to do other stuff.
+        /// </summary>
+        private void OpenNewWindow()
+        {
+            CreateWindow();
         }
     }
 }
